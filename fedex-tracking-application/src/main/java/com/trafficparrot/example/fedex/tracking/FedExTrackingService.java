aa5400408f8c284efd37890bac03be3f50547c01
Liam Williams <liam@trafficparrot.com>
2022-05-12 17:47:19 +0100

    Clean up imports

--- fedex-tracking-application/src/main/java/com/trafficparrot/example/fedex/tracking/FedExTrackingService.java
+++ fedex-tracking-application/src/main/java/com/trafficparrot/example/fedex/tracking/FedExTrackingService.java
@@ -10,8 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 import static com.jayway.jsonpath.Option.SUPPRESS_EXCEPTIONS;
 import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
