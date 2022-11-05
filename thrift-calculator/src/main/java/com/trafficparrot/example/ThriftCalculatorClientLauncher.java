aa5126ee11d8b067fc82005619c5f8829b3cf12e
Liam Williams <liam@trafficparrot.com>
2022-11-05 00:30:29 +0000

    Update to work with OpenJDK 11+ JavaFX

---
+++ thrift-calculator/src/main/java/com/trafficparrot/example/ThriftCalculatorClientLauncher.java
@@ -0,0 +1,11 @@
+package com.trafficparrot.example;
+
+/*
+ * Required as per: https://openjfx.io/openjfx-docs/#maven
+ */
+public class ThriftCalculatorClientLauncher {
+
+    public static void main(String[] args) {
+        ThriftCalculatorClient.main(args);
+    }
+}
---
+++ thrift-calculator/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.trafficparrot.example {
+    requires java.annotation;
+    requires javafx.controls;
+    requires org.apache.thrift;
+    requires org.slf4j;
+    exports com.trafficparrot.example;
+}
