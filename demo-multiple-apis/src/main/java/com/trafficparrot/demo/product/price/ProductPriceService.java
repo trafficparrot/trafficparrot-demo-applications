90fa996cf572f78cd1d476b12e37b291b3d46e47
Wojtek <wojtek@trafficparrot.com>
2023-02-23 15:17:23 +0100

    Support client RPC calls.

--- demo-multiple-apis/src/main/java/com/trafficparrot/demo/product/price/ProductPriceService.java
+++ demo-multiple-apis/src/main/java/com/trafficparrot/demo/product/price/ProductPriceService.java
@@ -1,34 +1,27 @@
 package com.trafficparrot.demo.product.price;
 
 import com.google.protobuf.InvalidProtocolBufferException;
-import org.springframework.amqp.core.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static com.trafficparrot.demo.product.price.ProductPriceQueueConfiguration.PRICE_REQUEST_QUEUE;
-import static com.trafficparrot.demo.product.price.ProductPriceQueueConfiguration.PRICE_RESPONSE_QUEUE;
 
 @Service
 public class ProductPriceService {
+    private static final Logger logger = LoggerFactory.getLogger(ProductPriceService.class);
 
-    private final RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    public ProductPriceService(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
-
-    @RabbitListener(queues = PRICE_REQUEST_QUEUE, ackMode = "AUTO")
-    public void listen(byte[] requestBytes) throws InvalidProtocolBufferException {
+    @RabbitListener(queues = PRICE_REQUEST_QUEUE)
+    public byte[] listen(byte[] requestBytes) throws InvalidProtocolBufferException {
         Price.PriceRequest priceRequest = Price.PriceRequest.parseFrom(requestBytes);
-        System.out.println("request = " + priceRequest);
+        logger.info("Request for price for product id: " + priceRequest.getProductId());
         Price.PriceResponse response = Price.PriceResponse.newBuilder()
                 .setProductId(priceRequest.getProductId())
                 .setPrice(1234)
                 .build();
-        byte[] responseBytes = response.toByteArray();
-        rabbitTemplate.send(PRICE_RESPONSE_QUEUE, new Message(responseBytes));
+        return response.toByteArray();
     }
 }
