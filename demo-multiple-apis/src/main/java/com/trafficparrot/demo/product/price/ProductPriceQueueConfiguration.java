c5547e3cda7919ec32698c16921a2258b5a62519
Liam Williams <liam@trafficparrot.com>
2023-02-24 13:35:52 +0000

    Remove unused queue

--- demo-multiple-apis/src/main/java/com/trafficparrot/demo/product/price/ProductPriceQueueConfiguration.java
+++ demo-multiple-apis/src/main/java/com/trafficparrot/demo/product/price/ProductPriceQueueConfiguration.java
@@ -8,15 +8,10 @@ import org.springframework.stereotype.Component;
 public class ProductPriceQueueConfiguration {
 
     public static final String PRICE_REQUEST_QUEUE = "price-request-queue";
-    public static final String PRICE_RESPONSE_QUEUE = "price-response-queue";
 
     @Bean
     public Queue priceRequestQueue() {
         return new Queue(PRICE_REQUEST_QUEUE, false);
     }
 
-    @Bean
-    public Queue priceResponseQueue() {
-        return new Queue(PRICE_RESPONSE_QUEUE, false);
-    }
 }
