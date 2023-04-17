5951563060029d42d9cc968141130756ebc46e3f
Liam Williams <liam@trafficparrot.com>
2023-04-17 11:48:43 +0100

    Added HTTP_SERVER_PORT, GRPC_SERVER_PORT, RABBITMQ_PRICE_REQUEST_QUEUE

--- demo-multiple-apis/src/main/java/com/trafficparrot/demo/product/price/ProductPriceQueueConfiguration.java
+++ demo-multiple-apis/src/main/java/com/trafficparrot/demo/product/price/ProductPriceQueueConfiguration.java
@@ -1,17 +1,21 @@
 package com.trafficparrot.demo.product.price;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductPriceQueueConfiguration {
 
-    public static final String PRICE_REQUEST_QUEUE = "price-request-queue";
+    public static final String PRICE_REQUEST_QUEUE = "${price.service.rabbitmq.request.queue.name}";
+
+    @Value(PRICE_REQUEST_QUEUE)
+    private String requestQueueName;
 
     @Bean
     public Queue priceRequestQueue() {
-        return new Queue(PRICE_REQUEST_QUEUE, false);
+        return new Queue(requestQueueName, false);
     }
 
 }
