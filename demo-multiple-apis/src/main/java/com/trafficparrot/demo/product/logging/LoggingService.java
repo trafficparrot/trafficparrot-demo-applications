23cca47bcb6ecfa9bc51625e76b54ab61b6b38e7
Liam Williams <liam@trafficparrot.com>
2023-04-23 17:21:39 +0100

    Show how multiple serialized Java-Proto types can be sent/received on the same queue

--- demo-multiple-apis/src/main/java/com/trafficparrot/demo/product/logging/LoggingService.java
+++ demo-multiple-apis/src/main/java/com/trafficparrot/demo/product/logging/LoggingService.java
@@ -1,40 +1,44 @@
 package com.trafficparrot.demo.product.logging;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.trafficparrot.demo.product.price.Price;
+import com.google.protobuf.GeneratedMessageV3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.function.Consumer;
 
 import static com.trafficparrot.demo.product.logging.LoggingQueueConfiguration.LOGGING_REQUEST_QUEUE;
-import static com.trafficparrot.demo.product.price.ProductPriceQueueConfiguration.PRICE_REQUEST_QUEUE;
-import static com.trafficparrot.demo.product.price.ProductPriceRepository.productPrice;
 
+/**
+ * Receiving/sending a Java serialized object over RabbitMQ, not just the Protobuf content, as per sample client use case.
+ */
 @Service
 public class LoggingService {
     private static final Logger logger = LoggerFactory.getLogger(LoggingService.class);
 
     @RabbitListener(queues = LOGGING_REQUEST_QUEUE)
-    public Logging.LoggingResponse listen(Logging.LoggingRequest loggingRequest) throws InvalidProtocolBufferException {
-        /*
-         Receiving/sending a Java serialized object over RabbitMQ, not just the Protobuf content, as per sample client use case.
-         */
+    public Logging.LoggingResponse listen(GeneratedMessageV3 loggingRequest) {
+        if (loggingRequest instanceof Logging.LoggingRequestDebug) {
+            Logging.LoggingRequestDebug loggingRequestDebug = (Logging.LoggingRequestDebug) loggingRequest;
+            return loggingResponse(logger::debug, loggingRequestDebug.getMessage());
+        } else if (loggingRequest instanceof Logging.LoggingRequestInfo) {
+            Logging.LoggingRequestInfo loggingRequestInfo = (Logging.LoggingRequestInfo) loggingRequest;
+            return loggingResponse(logger::info, loggingRequestInfo.getMessage());
+        } else if (loggingRequest instanceof Logging.LoggingRequestError) {
+            Logging.LoggingRequestError loggingRequestError = (Logging.LoggingRequestError) loggingRequest;
+            return loggingResponse(logger::error, loggingRequestError.getMessage());
+        } else {
+            throw new UnsupportedOperationException("Unsupported message type: " + loggingRequest.getClass().getName());
+        }
+    }
+
+    private static Logging.LoggingResponse loggingResponse(Consumer<String> logger, String message) {
         try {
-            if (loggingRequest.getLevel() == Logging.LOG_LEVEL.INFO) {
-                logger.info("Logging request from client '" + loggingRequest.getMessage() + "'");
-                return Logging.LoggingResponse.newBuilder()
-                        .setSuccess(Logging.LoggingResponseSuccess.newBuilder().build())
-                        .build();
-            } else {
-                return Logging.LoggingResponse.newBuilder()
-                        .setError(Logging.LoggingResponseError.newBuilder()
-                                .setMessage("Unsupported logging level: " + loggingRequest.getLevel())
-                                .build())
-                        .build();
-            }
+            logger.accept("Logging request from client '" + message + "'");
+            return Logging.LoggingResponse.newBuilder()
+                    .setSuccess(Logging.LoggingResponseSuccess.newBuilder().build())
+                    .build();
         } catch (Exception e) {
             return Logging.LoggingResponse.newBuilder()
                     .setError(Logging.LoggingResponseError.newBuilder().setMessage(e.getMessage()).build())
